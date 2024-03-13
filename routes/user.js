e1a45be321b0ab42b9eac82ca36e353028a022c2
zeineb-benmami <zeinebbenmami2016@gmail.com>
2024-03-13 14:49:49 +0100

    5th

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const {signup, getAllUsers, updateUser,deleteUser, signin, addAdmin,blockUser,unBlockUser,getUserProfile,getuser} = require("../controllers/userController");
+const {signup,
+     getAllUsers,
+     updateUser,
+     deleteUser,
+     signin,
+     addAdmin,
+     blockUser,
+     unBlockUser,
+     getUserProfile,
+     getuser,
+     addTRM,
+     addTM
+    } = require("../controllers/userController");
 const {userValidator} = require("../middlewares/validators");
 
 
 router.get("/getall", getAllUsers);
 router.post("/signup", userValidator, signup);
-router.put("/update", userValidator, updateUser);
+router.put("/update/:id", userValidator, updateUser);
 router.delete("/delete/:id", deleteUser);
 router.post("/signin",signin);
 router.post("/addAdmin",addAdmin);
@@ -14,5 +26,6 @@ router.patch("/block/:id", blockUser);
 router.patch("/unblock/:id", unBlockUser);
 router.get('/profile', getUserProfile);
 router.get("/getuser/:id",getuser);
-
+router.post("/addTRM",addTRM);
+router.post("/addTM",addTM);
 module.exports = router;
