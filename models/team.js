c9bf6913b2be3db3e51c3012c520bdd1a31514b9
sirineghribi <sirine.ghribi@esprit.tn>
2024-03-24 22:36:56 +0100

    sirine

--- models/team.js
+++ models/team.js
@@ -1,5 +1,6 @@
 const mongo = require("mongoose");
 const Schema = mongo.Schema;
+
 const Team = new Schema({
     name: String,
     nameAbbreviation: String,
@@ -11,13 +12,23 @@ const Team = new Schema({
     description: String,
     slogan: String,
     nickname: String,
+    capital:Number,
     wins: { type: Number, default: 0 },
-    losses:{ type: Number, default: 0 },
+    losses: { type: Number, default: 0 },
     draws: { type: Number, default: 0 },
     trophies: { type: [], default: [] },
     ranking: { type: Number, default: 1000 },
-    creator:{ type: Schema.Types.ObjectId, ref: 'User' },
-    managers:[{ type: Schema.Types.ObjectId, ref: 'User' }],
-    tournaments: [{ type: Schema.Types.ObjectId, ref: 'Tournament' }]
+    creator: { type: Schema.Types.ObjectId, ref: 'User' },
+    managers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    tournaments: [{ type: Schema.Types.ObjectId, ref: 'Tournament' }],
+    sponsors: [{
+        name:String,
+        description:String,
+        logo:String,
+        contact:Number,
+        adresse:String,
+     
+    }]
 });
+
 module.exports = mongo.model("team", Team);
